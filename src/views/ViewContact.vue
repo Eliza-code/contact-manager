23112ef7185a6e8a96020e4165800a7eb70454be
Eliza-code <lizcano.liz14@gmail.com>
2022-07-31 17:13:53 -0500

    create view contact component

--- src/views/ViewContact.vue
+++ src/views/ViewContact.vue
@@ -1,5 +1,32 @@
 <template>
- <h2>View Contact</h2>
+ <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <p class=" h3 text-success fw-bold">View Contact</p>
+        <p class="fst-italic">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-md-4">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6rbCgaHI6TkSkS2Jzfggu2Z7FHcXBQJU2fEQmW7aSbJN_RTMR84Uf_wAAGKIiHRzU5Mc&usqp=CAU" alt="" class="contact-img">
+      </div>
+      <div class="col-md-6">
+        <ul class="list-group">
+          <li class="list-group-item">Name: <span class="fw-bold">Name</span></li>
+          <li class="list-group-item">Email: <span class="fw-bold">Email</span> </li>
+          <li class="list-group-item">Mobile: <span class="fw-bold">Mobile</span> </li>
+          <li class="list-group-item">Group: <span class="fw-bold">Group</span> </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <router-link to="/" class="btn btn-success "><i class="fa fa-arrow-alt-circle-left"></i> Back</router-link>
+      </div>
+    </div>
+  </div>
 
 </template>
 <script>
